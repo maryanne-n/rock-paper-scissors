a3c78c122a4299953105c42b3cd287a0eb4877f6
maryanne-n <mng8hc@gmail.com>
2022-05-08 20:53:06 +1000

    Add basic result comments. Need to add final scorecard before alert modal

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ let computerSelection;
 let roundCount = 0;
 let playerScore = 0;
 let computerScore = 0;
-const item = document.getElementsByTagName(".item");
-// console.log(roundCount, playerScore, computerScore)
-// const playerSelection = "rock";
+const result = document.getElementById("result");
+const winner = document.getElementById("winner");
+const loser = document.getElementById("loser");
+const middle = document.getElementById("middle");
 
 // generate random computer input
 function computerPlay() {
@@ -38,22 +39,34 @@ function getPlayerSelection(e) {
 
 // compare playerSelection vs computerSelection and keeps track of score
 function playRound(playerSelection, computerSelection) {
-
+        result.textContent = "";
+        winner.textContent = "";
+        loser.textContent = "";
+        middle.style.visibility = "hidden";
         if (playerSelection === computerSelection ) {
             console.log("tie!");
             console.log("player score is " + playerScore, "computer score is " + computerScore);
+            result.textContent = "It's a tie.";
         }
 
         if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')) {
             console.log("Player wins this round`!");
             playerScore++;
             console.log("player score is " + playerScore, "computer score is " + computerScore);
+            result.textContent = "You win!";
+            winner.textContent = playerSelection;
+            loser.textContent = computerSelection;
+            middle.style.visibility = "visible";
         }
 
         if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === 'paper' && playerSelection === 'rock')) {
             console.log("Computer wins this round!");            
             computerScore++;
             console.log("player score is " + playerScore, "computer score is " + computerScore);
+            result.textContent = "You lose!";
+            winner.textContent = computerSelection;
+            loser.textContent = playerSelection;
+            middle.style.visibility = "visible";
         }
 
         roundCount++;
@@ -68,9 +81,19 @@ function playRound(playerSelection, computerSelection) {
 
 // reset everything for a new game
 function gameOver() {
-    alert("Game Over! Would you like to try again?");
+    if (computerScore == playerScore) {
+        alert("It's a tie! Would you like to try again?");
+    } else if (computerScore > playerScore) {
+        alert("You lost! Would you like to try again?");
+    } else if (playerScore > computerScore) {
+        alert("You won! Would you like to try again?");
+    }
+    // alert("Game Over! Would you like to try again?");
     roundCount = 0;
     playerScore = 0;
     computerScore = 0;  
-    
+    result.textContent = "";
+    winner.textContent = "";
+    loser.textContent = "";
+    middle.style.visibility = "hidden";                                     
 }
