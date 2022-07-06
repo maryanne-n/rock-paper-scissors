62149351ed6c7833d50400240c11d13f7c9d3ca0
maryanne-n <mng8hc@gmail.com>
2022-07-07 09:15:13 +1000

    Main game flow works. UI improvements will be worked on in upcoming pushes

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const scissors = document.querySelector(".scissors");
 let playerSelection;
 let computerSelection;
 let roundCount = 0;
+// let playerScore = document.querySelector(".player-score");
+// let computerScore = document.querySelector(".computer-score");
 let playerScore = 0;
 let computerScore = 0;
+
 const result = document.getElementById("result");
 
 // generate random computer input
@@ -13,10 +16,13 @@ function computerPlay() {
     randomNum = Math.floor(Math.random() * 3);    
     switch(randomNum) {
         case 0: 
-            return 'rock';
+            document.getElementById("computer-img").src="images/rock-c.png";
+            return 'rock';            
         case 1:
+            document.getElementById("computer-img").src="images/paper-c.png";
             return 'paper';            
         case 2: 
+            document.getElementById("computer-img").src="images/scissors-c.png";
             return 'scissors';
     }
 }
@@ -29,6 +35,14 @@ document.querySelectorAll(".item").forEach(function(e) {
 // triggers the comparison of playerSelection vs computerSelection
 function getPlayerSelection(e) {
     playerSelection = event.target.id;
+                   
+        if (playerSelection == 'rock') {
+            document.getElementById('player-img').src= "images/rock.png";
+        } else if (playerSelection == 'paper') {
+            document.getElementById('player-img').src = "images/paper.png";
+        } else {
+            document.getElementById('player-img').src = "images/scissors.png";
+        }
     computerSelection = computerPlay();
     console.log(playerSelection, computerSelection);
     playRound(playerSelection, computerSelection);        
@@ -46,6 +60,7 @@ function playRound(playerSelection, computerSelection) {
         if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')) {
             console.log("Player wins this round`!");
             playerScore++;
+            document.querySelector(".player-score").textContent = playerScore;
             console.log("player score is " + playerScore, "computer score is " + computerScore);
             result.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
         }
@@ -53,6 +68,7 @@ function playRound(playerSelection, computerSelection) {
         if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === 'paper' && playerSelection === 'rock')) {
             console.log("Computer wins this round!");            
             computerScore++;
+            document.querySelector(".computer-score").textContent = computerScore;
             console.log("player score is " + playerScore, "computer score is " + computerScore);
             result.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
         }
@@ -62,7 +78,8 @@ function playRound(playerSelection, computerSelection) {
 
         if (roundCount == 5) {
             console.log("GAME OVER! " + " Final score is " + playerScore + " for the player and " + computerScore + " for the computer")
-            gameOver();
+            // gameOver();
+            const timeout = setTimeout(gameOver, 1000)
         }
 
 }
@@ -70,15 +87,19 @@ function playRound(playerSelection, computerSelection) {
 // reset everything for a new game
 function gameOver() {
     if (computerScore == playerScore) {
-        alert("It's a tie! Would you like to try again?");
+        alert("Final score is " + playerScore + " for the player and " + computerScore + " for the computer. It's a tie! Would you like to try again?");
     } else if (computerScore > playerScore) {
-        alert("You lost! Would you like to try again?");
+        alert("Final score is " + playerScore + " for the player and " + computerScore + " for the computer. You lost! Would you like to try again?");
     } else if (playerScore > computerScore) {
-        alert("You won! Would you like to try again?");
+        alert("Final score is " + playerScore + " for the player and " + computerScore + " for the computer. You won! Would you like to try again?");
     }
     // alert("Game Over! Would you like to try again?");
     roundCount = 0;
     playerScore = 0;
     computerScore = 0;  
+    document.querySelector(".player-score").textContent = 0;
+    document.querySelector(".computer-score").textContent = 0;
+    document.getElementById('player-img').src = "";
+    document.getElementById('computer-img').src = "";
     result.textContent = "";                                
 }
