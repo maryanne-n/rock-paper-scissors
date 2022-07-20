bacc7a0e306bbbe30cd526b913f7cecb96cfa409
maryanne-n <mng8hc@gmail.com>
2022-07-20 12:59:40 +1000

    Add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project Rock Paper Scissors for The Odin Project (TOP)
+
+I coded a simple 5-round game of the popular Rock Paper Scissors created as part of The Odin Project (TOP) curriculum. 
+
+[Here is the brief](https://www.theodinproject.com/lessons/foundations-rock-paper-scissors). There were many impressive solutions, however most opted to present the game as a "First to 5 points wins". For my version, I have chosen to keep it strictly to a 5-round game, according to instructions.
+
+:boom: Live demo is up as of 20 July 2022! [Here's the site](https://rockpaperscissors-ma.netlify.app/). :boom:
--- script.js
+++ script.js
@@ -8,8 +8,6 @@ tryBtn.style.display = "none";
 let playerSelection;
 let computerSelection;
 let roundCount = 0;
-// let playerScore = document.querySelector(".player-score");
-// let computerScore = document.querySelector(".computer-score");
 let playerScore = 0;
 let computerScore = 0;
 
@@ -49,42 +47,34 @@ function getPlayerSelection(e) {
             document.getElementById('player-img').src = "images/scissors.png";
         }
     computerSelection = computerPlay();
-    console.log(playerSelection, computerSelection);
     playRound(playerSelection, computerSelection);        
 }
 
 // compare playerSelection vs computerSelection and keeps track of score
 function playRound(playerSelection, computerSelection) {
-        result.textContent = "";
-        if (playerSelection === computerSelection ) {
-            console.log("tie!");
-            console.log("player score is " + playerScore, "computer score is " + computerScore);
-            result.textContent = "It's a tie.";
-        }
+        result.textContent = `Round ${roundCount + 1}: `;
+    if (playerSelection === computerSelection ) {
+        result.textContent += "It's a tie.";
+    }
+    if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')) {
+        playerScore++;
+        document.querySelector(".player-score").textContent = playerScore;
+        result.textContent += `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === 'paper' && playerSelection === 'rock')) {         
+        computerScore++;
+        document.querySelector(".computer-score").textContent = computerScore;
+        result.textContent += `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
 
-        if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')) {
-            console.log("Player wins this round`!");
-            playerScore++;
-            document.querySelector(".player-score").textContent = playerScore;
-            console.log("player score is " + playerScore, "computer score is " + computerScore);
-            result.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-        }
+    roundCount++;
+    console.log(roundCount);
 
-        if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === 'paper' && playerSelection === 'rock')) {
-            console.log("Computer wins this round!");            
-            computerScore++;
-            document.querySelector(".computer-score").textContent = computerScore;
-            console.log("player score is " + playerScore, "computer score is " + computerScore);
-            result.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
-        }
-
-        roundCount++;
-        console.log(roundCount);
-
-        if (roundCount == 5) {
-            console.log("GAME OVER! " + " Final score is " + playerScore + " for the player and " + computerScore + " for the computer")
-            gameOver();
-        }
+    if (roundCount == 5) {
+        // Grey out the user playboard temporarily to signify game is inactive
+        tools.classList.add("inactive");
+        setTimeout(gameOver, 2000);
+    }
 
 }
 
@@ -92,30 +82,35 @@ function playRound(playerSelection, computerSelection) {
 function gameOver() {
     if (computerScore == playerScore) {
         result.textContent = "It's a tie! Final score is " + playerScore + " for the player and " + computerScore + " for the computer. Would you like to try again?";
+
     } else if (computerScore > playerScore) {
-        result.textContent = "You lost! Final score is " + playerScore + " for the player and " + computerScore + " for the computer. Would you like to try again?";
+        result.textContent = "You lost the game! Final score is " + playerScore + " for the player and " + computerScore + " for the computer. Would you like to try again?";
+
     } else if (playerScore > computerScore) {
-        result.textContent = "You won! Final score is " + playerScore + " for the player and " + computerScore + " for the computer. Would you like to try again?";
+        result.textContent = "You won the game! Final score is " + playerScore + " for the player and " + computerScore + " for the computer. Would you like to try again?";
+
     }
-    // Remove pointer events temporarily
-    // tools.style.pointerEvents = "none";
-    tools.classList.add("inactive");
-    // Clear the scoreboard selections
+
+    // Remove the scoreboard selections
     document.querySelector(".choice-p").style.display = "none";
     document.querySelector(".choice-c").style.display = "none";
+
     // Reveal the try again button
     tryBtn.style.display = "block";                                
 }
 
 function reStart() {
-    // Restore pointer events
-    // tools.style.pointerEvents = "auto";
-    tools.classList.remove("inactive");
-    // Hide the try again button
-    tryBtn.style.display = "none";
+    // Restore user playboard
+    tools.classList.remove("inactive");    
+
     // Restore the scoreboard selections
     document.querySelector(".choice-p").style.display = "flex";
     document.querySelector(".choice-c").style.display = "flex";
+
+    // Hide the try again button
+    tryBtn.style.display = "none";
+
+    // Reset player/computer scores and selections
     roundCount = 0;
     playerScore = 0;
     computerScore = 0;  
